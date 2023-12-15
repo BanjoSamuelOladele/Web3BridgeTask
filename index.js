efcefe1c482dfc6015fb5ea38f2a4614f0b06abc
Banjo Oladele Samuel <o.samuel@native.semicolon.africa>
2023-12-15 12:27:23 +0100

    web3bridge quiz task

--- index.js
+++ index.js
@@ -9,16 +9,41 @@ const resultTextElement = document.getElementById('showResult');
 
 const questions = [
     {
-        question: 'What is the capital of France?',
-        answers: ['Paris', 'Berlin', 'London'],
-        correctAnswer: 'Paris'
+      question: "What does CSS stand for?",
+      options: ["Computer Style Sheets", "Creative Style Sheets", "Cascading Style Sheets"],
+      answer: "Cascading Style Sheets",
     },
     {
-        question: 'Which programming language is used for web development?',
-        answers: ['Java', 'Python', 'JavaScript'],
-        correctAnswer: 'JavaScript'
-    }
-];
+      question: "Which programming language is often used for web development?",
+      options: ["Java", "Python", "JavaScript"],
+      answer: "JavaScript",
+    },
+    {
+      question: "What does HTML stand for?",
+      options: ["Hyper Transfer Markup Language", "Hyper Text Markup Language", "High-level Text Markup Language"],
+      answer: "Hyper Text Markup Language",
+    },
+    {
+      question: "What is the purpose of the 'git clone' command?",
+      options: ["To create a new Git repository", "To make a copy of an existing repository", "To commit changes to a repository"],
+      answer: "To make a copy of an existing repository",
+    },
+    {
+      question: "In Python, which of the following is used to comment a single line?",
+      options: ["// This is a comment", "# This is a comment", "/* This is a comment */"],
+      answer: "# This is a comment",
+    },
+    {
+      question: "What is the result of the expression '5 + '5'' in JavaScript?",
+      options: ["55", "10", "'55'"],
+      answer: "'55'",
+    },
+    {
+      question: "What is the purpose of the SQL SELECT statement?",
+      options: ["To insert data into a database", "To update existing data in a database", "To retrieve data from a database"],
+      answer: "To retrieve data from a database",
+    },
+  ];
 
 function startQuiz() {
     showQuestion(currentQuestionIndex);
@@ -29,7 +54,7 @@ function showQuestion(index) {
     questionTextElement.innerText = question.question;
     answerButtonsElement.innerHTML = '';
 
-    question.answers.forEach(option => {
+    question.options.forEach(option => {
         const button = document.createElement('button');
         button.innerText = option;
         button.classList.add('clickNext');
@@ -75,7 +100,7 @@ function showResult() {
 function calculateScore() {
     let score = 0;
     for (let i = 0; i < questions.length; i++) {
-        if (userAnswers[i] === questions[i].correctAnswer) {
+        if (userAnswers[i] === questions[i].answer) {
             score++;
         }
     }
